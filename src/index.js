88e38420de6cd362647d1ead9a3c847712bdcf11
StaloneSerhii <sergio.fly4405@gmail.com>
2022-10-15 00:34:24 +0300

    js con

--- src/index.js
+++ src/index.js
@@ -1,34 +1,4 @@
-// Contacts map-modal.js
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-map-modal-open]"),
-    closeModalBtn: document.querySelector("[data-map-modal-close]"),
-    modal: document.querySelector("[data-map-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-
-// Contacts franchise-modal.js
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-franch-modal-open]"),
-    closeModalBtn: document.querySelector("[data-franch-modal-close]"),
-    modal: document.querySelector("[data-franch-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+import './js/contacts';
 import './js/anchrol-scroll';
 import './js/header-scroll';
 
---
+++ src/js/contacts.js
@@ -0,0 +1,31 @@
+// Contacts map-modal.js
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-map-modal-open]"),
+    closeModalBtn: document.querySelector("[data-map-modal-close]"),
+    modal: document.querySelector("[data-map-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+// Contacts franchise-modal.js
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-franch-modal-open]"),
+    closeModalBtn: document.querySelector("[data-franch-modal-close]"),
+    modal: document.querySelector("[data-franch-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
