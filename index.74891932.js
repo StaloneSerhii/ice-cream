7472f9c794d8e8641ae2a8741b80f7ac76c44f4e
StaloneSerhii <110424622+StaloneSerhii@users.noreply.github.com>
2022-10-14 21:08:05 +0000

    Deploying to gh-pages from @ StaloneSerhii/ice-cream@acbf467f93b9b63628523bb744e8d8f11050df3e 🚀

--- index.74891932.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$("a").on("click",(function(e){if(""!==this.hash){e.preventDefault();var a=this.hash;$("html, body").animate({scrollTop:$(a).offset().top},800,(function(){window.location.hash=a}))}}))}));const e={headerEl:document.querySelector(".header-js")};window.onscroll=function(){window.screen.width>=768?scrollY<=70?e.headerEl.classList.remove("header-bg"):e.headerEl.classList.add("header-bg"):scrollY<=120?e.headerEl.classList.remove("header-bg"):e.headerEl.classList.add("header-bg")};
-//# sourceMappingURL=index.74891932.js.map
