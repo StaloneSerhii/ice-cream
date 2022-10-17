c7eb15126d280eb4271e660844287631829317a9
Oleksandr <110254286+OleksandrUA777@users.noreply.github.com>
2022-10-17 18:40:34 +0300

    Revert "fix bug" (#38)
    Co-authored-by: Serhii <110424622+StaloneSerhii@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './js/anchrol-scroll';
 import './js/header-scroll';
 import './js/slider';
 import './js/gallery';
+import './js/header-modal';
+// import './js/mobile-navigation'
 // import './js/header-modal';
 import './js/reviews-slide';
 // import './js/mobile-navigation'
