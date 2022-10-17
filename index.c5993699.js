93752a6842623937818e5ac42dcd3bd323530133
StaloneSerhii <110424622+StaloneSerhii@users.noreply.github.com>
2022-10-17 18:55:46 +0000

    Deploying to gh-pages from @ StaloneSerhii/ice-cream@335c9d26b571fde2ffb243cd9d31856e2e98b628 🚀

---
+++ index.c5993699.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[products-modal-open]"),closeModalBtn:document.querySelector("[products-modal-close]"),modal:document.querySelector("[products-modal]")};function e(){o.modal.classList.toggle("products-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.c5993699.js.map
