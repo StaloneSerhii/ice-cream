08cad3ad666f5a6eb54cd36500a7aa312b291924
StaloneSerhii <sergio.fly4405@gmail.com>
2022-10-18 22:46:00 +0300

    s

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './js/contacts';
 import './js/anchrol-scroll';
 import './js/header-scroll';
 import './js/slider';
-// import './js/hero-modal';
+// import './js/hero-modal;
 import './js/header-modal';
 // import './js/about-modal';
 // import './js/reviews-slide';
