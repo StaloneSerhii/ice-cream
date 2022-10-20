ca576add9511540a06531c62f8bf08902083bbb1
Kostiantyn-Demchyshyn <110294010+Kostiantyn-Demchyshyn@users.noreply.github.com>
2022-10-20 20:57:22 +0100

    add header js

--- src/js/hero-modal.js
+++ src/js/hero-modal.js
@@ -1,14 +1,17 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[hero-modal-open]"),
-      closeModalBtn: document.querySelector("[hero-modal-close]"),
-      modal: document.querySelector("[hero-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+  const refs = {
+    closeModalBtn: document.querySelector('[hero-modal-close]'),
+    modal: document.querySelector('[hero-modal]'),
+  };
+
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+  document.addEventListener('click', e => {
+    if (e.target.matches('[hero-modal-open]')) {
+      toggleModal();
     }
-  })();
+  });
+})();
